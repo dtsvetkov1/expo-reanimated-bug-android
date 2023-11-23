c33ec9248f6c2e351a672757f6b8cb2ca1342e41
Danila Tsvetkov <danilatsvet@gmail.com>
2023-11-24 02:49:53 +0400

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
+import BottomSheet from '@gorhom/bottom-sheet';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
 
 export default function App() {
+  const animValue = useSharedValue(0);
+
+  useEffect(() => {
+    animValue.value = withRepeat(withTiming(1, { duration: 1000 }), 5);
+  }
+  , []);
+
+  const animStyle = useAnimatedStyle(() => {
+    return {
+      // width: animValue.value * 255,
+      transform: [{ translateX: animValue.value * 255 }],
+    };
+  }
+  );
+
+
   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Animated.View style={[styles.block, animStyle]} />
+      <BottomSheet
+        // ref={bottomSheetRef}
+        index={1}
+        snapPoints={[100, 200]}
+        // onChange={handleSheetChanges}
+      >
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheet>
     </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -16,5 +49,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  block: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
   },
 });
---
+++ README.md
@@ -0,0 +1,60 @@
+# About
+
+This project is a minimal reproducible example of a bug in the Expo SDK.
+
+# Steps to reproduce
+
+run
+
+```
+npm install
+```
+
+then
+
+```
+npm start
+```
+
+The project will work correctly.
+
+Then create a release build (for example)
+
+```
+cd android && ./gradlew assembleRelease
+```
+
+and install it on an Android device. Reanimated animations will not work (the problem may not appear on the first run. If you close the app and open it again, the problem will appear in 95% of cases)
+
+## Possible solution
+
+In the `MainActivity.java` file, replace
+
+```
+
+@Override
+protected ReactActivityDelegate createReactActivityDelegate() {
+return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+new DefaultReactActivityDelegate(
+this,
+getMainComponentName(),
+// If you opted-in for the New Architecture, we enable the Fabric Renderer.
+DefaultNewArchitectureEntryPoint.getFabricEnabled()));
+}
+
+```
+
+with
+
+```
+
+@Override
+protected ReactActivityDelegate createReactActivityDelegate() {
+return new DefaultReactActivityDelegate(
+this,
+getMainComponentName(),
+// If you opted-in for the New Architecture, we enable the Fabric Renderer.
+DefaultNewArchitectureEntryPoint.getFabricEnabled());
+}
+
+```
